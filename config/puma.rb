8761ab742e030801a51ef76d72c9c454beb9d657
xiuce <xiuce123@126.com>
2018-03-13 11:18:21 +0800

    cms_docker

---
+++ config/puma.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env puma
+
+#rails的运行环境
+environment 'production'
+threads 2, 8
+workers 4
+port 3000
+#项目名
+app_name         = "one_cms"
+#项目路径
+application_path = "/var/www/#{app_name}"
+#这里一定要配置为项目路径下地current
+directory "#{application_path}"
+
+#下面都是 puma的配置项
+pidfile "#{application_path}/tmp/pids/puma.pid"
+state_path "#{application_path}/tmp/sockets/puma.state"
+stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
+bind "unix://#{application_path}/tmp/sockets/puma.sock"
+activate_control_app "unix://#{application_path}/tmp/sockets/pumactl.sock"
+
+#后台运行
+# daemonize true
+on_restart do
+  puts 'On restart...'
+end
+# preload_app!
